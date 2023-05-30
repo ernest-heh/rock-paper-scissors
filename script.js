a62787a3d4804bd10c040bd9fd5056ff42d72ab3
Ernest Ho <ernest.heh@gmail.com>
2023-05-30 17:26:02 +0800

    initialize project

---
+++ script.js
@@ -0,0 +1,17 @@
+// Rock Paper Scissors Logic
+// Player Selects: Rock | Paper | Scissors
+// Computer Selects: ['Rock', 'Paper', 'Scissors'] random
+
+// if Player: Rock && Computer: Scissors, Player + 1
+// if Player: Paper && Computer: Rock, Player + 1
+// if Player: Scissors && Computer: Paper, Player + 1
+// if Player: Rock && Computer: Paper, Computer + 1
+// if Player: Paper && Computer: Scissors, Computer + 1
+// if Player: Scissors && Computer: Rock, Computer + 1
+// if Player: === Computer:, Draw
+
+// if playerScore === 5, Player Wins
+// if computerScore === 5, Computer Wins
+const play = document.getElementById("play");
+
+console.log("Hello from script.js!");
